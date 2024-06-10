07a8af0e7eef307f8e2abfaced65092644695439
Ashutosh sharma <96047256+Ashutosh137@users.noreply.github.com>
2024-06-10 17:50:31 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import UseProvider from "@/lib/store/store";
 const inter = Inter({ subsets: ["latin"] });
-// export const metadata = {
-//   title: "Quotverse",
-//   description: "Quotverse",
-// };
 
+
+export const metadata = {
+  title: "Quotverse: Your World of Quotes",
+  description: "Quotverse is a dynamic web application designed to immerse users in the inspiring and thought-provoking world of quotes. Whether you’re seeking motivation, wisdom, or simply a beautiful phrase to brighten your day, Quotverse provides a seamless and interactive experience for discovering, managing, and sharing quotes.",
+};
 
 export default function RootLayout({ children }) {
   return (
